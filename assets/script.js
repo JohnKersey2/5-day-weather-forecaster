312211c498f332105288fe41ea0c4030340a7417
John Kersey <johnkersey2@yahoo.com>
2023-07-09 18:37:54 -0400

    .

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,6 @@
+var searchBtn = document.querySelector('#searchBtn');
+var cityInput = document.querySelector('#city');
+var searchArea = document.querySelector('#search');
+
 var today = dayjs().format('M/D/YYYY');
 console.log(today)
